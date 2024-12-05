500c8523bedc5869748c1493821d262bba5ffd8d
parksang-won <tkddnjs2575@naver.com>
2024-12-06 08:53:17 +0900

    [추가] Tabbar에 따라 AppBar 색상 변경

--- lib/presentation/pages/main/component/top_app_bar/home_app_bar.dart
+++ lib/presentation/pages/main/component/top_app_bar/home_app_bar.dart
@@ -1,57 +1,84 @@
-
-
+import 'package:e_commerce_app/core/theme/custom/custom_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../../../../core/theme/constant/app_icons.dart';
+import '../../cubit/mall_type_cubit.dart';
 
 class HomeAppBar extends StatelessWidget {
   const HomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PreferredSize(
-      preferredSize: Size.fromHeight(44),
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-        color: Theme.of(context).colorScheme.primary,
-        child: AppBar(
-          leading: Container(
-            padding: EdgeInsets.all(8),
-            color: Theme.of(context).colorScheme.primary,
-            child: SvgPicture.asset(
-              AppIcons.mainLogo,
+    return BlocBuilder<MallTypeCubit, MallType>(
+      builder: (context, state) {
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+          color: (state.isMarket)
+              ? Theme.of(context).colorScheme.primary
+              : Theme.of(context).colorScheme.surface,
+          child: AppBar(
+            leading: Container(
+              padding: EdgeInsets.all(8),
+              child: SvgPicture.asset(
+                AppIcons.mainLogo,
+                colorFilter: ColorFilter.mode(
+                  (state.isMarket)
+                      ? Theme.of(context).colorScheme.onPrimary
+                      : Theme.of(context).colorScheme.primary,
+                  BlendMode.srcIn,
+                ),
+              ),
             ),
+            title: DefaultTabController(
+              initialIndex: state.index,
+              length: MallType.values.length,
+              child: TabBar(
+                onTap: (index) =>
+                    context.read<MallTypeCubit>().changeIndex(index),
+                labelColor: Theme.of(context).colorScheme.surface,
+                unselectedLabelColor: Theme.of(context).colorScheme.onPrimary,
+                tabs: List.generate(
+                  MallType.values.length,
+                  (index) => Tab(text: MallType.values[index].label),
+                ),
+              ),
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: SvgPicture.asset(
+                  AppIcons.location,
+                  width: 32,
+                  colorFilter: ColorFilter.mode(
+                    (state.isMarket)
+                        ? Theme.of(context).colorScheme.surface
+                        : Theme.of(context).colorScheme.contentPrimary,
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: SvgPicture.asset(
+                  AppIcons.cart,
+                  width: 32,
+                  colorFilter: ColorFilter.mode(
+                    (state.isMarket)
+                        ? Theme.of(context).colorScheme.surface
+                        : Theme.of(context).colorScheme.contentPrimary,
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
+            ],
+            backgroundColor: Colors.transparent,
+            leadingWidth: 86,
           ),
-          title: const Text('TabBar'),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: SvgPicture.asset(
-                AppIcons.location,
-                width: 32,
-                colorFilter: ColorFilter.mode(
-                  Theme.of(context).colorScheme.surface,
-                  BlendMode.srcIn,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: SvgPicture.asset(
-                AppIcons.cart,
-                width: 32,
-                colorFilter: ColorFilter.mode(
-                  Theme.of(context).colorScheme.surface,
-                  BlendMode.srcIn,
-                ),
-              ),
-            ),
-          ],
-          backgroundColor: Colors.transparent,
-          leadingWidth: 86,
-        ),
-      ),
+        );
+      },
     );
   }
 }
--- lib/presentation/pages/main/main_screen.dart
+++ lib/presentation/pages/main/main_screen.dart
@@ -2,21 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import '../../../core/theme/constant/app_icons.dart';
 import '../category/category_page.dart';
 import '../home/home_page.dart';
 import '../search/search_page.dart';
 import '../user/user_page.dart';
 import 'component/top_app_bar/top_app_bar.dart';
 import 'cubit/bottom_nav_cubit.dart';
+import 'cubit/mall_type_cubit.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => BottomNavCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => BottomNavCubit(),
+        ),
+        BlocProvider(
+          create: (_) => MallTypeCubit(),
+        ),
+      ],
       child: MainScreenView(),
     );
   }
@@ -45,14 +52,14 @@ class MainScreenView extends StatelessWidget {
       ),
       bottomNavigationBar: BlocBuilder<BottomNavCubit, BottomNav>(
         builder: (context, state) {
-
           return BottomNavigationBar(
             items: List.generate(
               BottomNav.values.length,
               (index) => BottomNavigationBarItem(
                 icon: SvgPicture.asset(BottomNav.values[index].icon),
                 label: BottomNav.values[index].label,
-                activeIcon: SvgPicture.asset(BottomNav.values[index].activeIcon),
+                activeIcon:
+                    SvgPicture.asset(BottomNav.values[index].activeIcon),
               ),
             ),
             onTap: (index) => context.read<BottomNavCubit>().changeIndex(index),
